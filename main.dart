ac23fb5932b1a78e19ca4f3f16cc61fe2511d792
Abhishek1108 <52409610+Abhishek1108@users.noreply.github.com>
2020-05-23 21:46:47 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flipkart/pages/secondpage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(MaterialApp(
+    //2000   title:'flipkart',
+    home: secondpage(),
+  ));
+}
